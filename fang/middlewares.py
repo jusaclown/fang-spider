5f55f82f63d7fa74dd09e694fce61ae90d1c50c7
jusaclown <zjtxdy666@163.com>
2020-03-26 19:57:41 +0800

    first commit

---
+++ fang/middlewares.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+from scrapy import signals
+import random 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from scrapy.http.response.html import HtmlResponse
+
+class UserAgentDownloadMiddleware(object):
+    
+    USER_AGENTS = [
+        'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; Acoo Browser 1.98.744; .NET CLR 3.5.30729)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; America Online Browser 1.1; Windows NT 5.1; (R1 1.5); .NET CLR 2.0.50727; InfoPath.1)',
+        'Mozilla/5.0 (compatible; MSIE 9.0; AOL 9.7; AOLBuild 4343.19; Windows NT 6.1; WOW64; Trident/5.0; FunWebProducts)',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/44.0.2403.155 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; WOW64; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.0.04506; Media Center PC 5.0; .NET CLR 3.5.21022; GreenBrowser)',
+        'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
+        'Mozilla/5.0 (Windows; U; Win 9x 4.90; SG; rv:1.9.2.4) Gecko/20101104 Netscape/9.1.0285',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0',
+        'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'
+    ]
+    
+    def process_request(self, request, spider):
+        user_agent = random.choice(self.USER_AGENTS)
+        request.headers['User-Agent'] = user_agent
+
+class SeleniumDownloadMiddleware(object):
+    def __init__(self):
+        self.browser = webdriver.Firefox()
+        
+    def process_request(self, request, spider):
+        self.browser.get(request.url)
+        try:  
+            wait = WebDriverWait(self.browser, 1)
+            #find_newhouse = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@id="newhouse_loupai_list"]//li')))
+        except Exception as e:
+            print('='*80)
+            print("ERROR:", e)
+            print('='*80)
+        source = self.browser.page_source
+        response = HtmlResponse(url=self.browser.current_url, body=source, 
+                                request=request, encoding='utf-8')
+        return response    
---
+++ fang/spiders/fangtianxia.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+from fang.items import NewHouseItem, ESFItem
+
+class FangtianxiaSpider(scrapy.Spider):
+    name = 'fangtianxia'
+    allowed_domains = ['fang.com']
+    start_urls = ['https://www.fang.com/SoufunFamily.htm']
+
+    def parse(self, response):
+        trs = response.xpath('//div[@class="outCont"]//tr')
+        province = None
+        for tr in trs:
+            tds = tr.xpath(".//td[not(@class)]")
+            province_td = tds[0]
+            province_text = province_td.xpath('.//text()').get()    
+            province_text = re.sub(r"\s", "", province_text)
+            if province_text:
+                province = province_text
+
+            if province == "其它" or province == "台湾":
+                continue
+            city_td = tds[1]
+            city_links = city_td.xpath(".//a")
+            for city_link in city_links:
+                city = city_link.xpath(".//text()").get()
+                city_url = city_link.xpath(".//@href").get()
+               
+                url_module = city_url.split(".", 1)
+                if 'bj' in url_module[0]:
+                    newshouse_url = "https://newhouse.fang.com/house/s/"
+                    esf_url = "https://esf.fang.com/"
+                else:
+                    # 构建新房的url链接
+                    newshouse_url = url_module[0] + '.newhouse.' + url_module[1] + 'house/s'
+                    # 构建二手房的url链接
+                    esf_url = url_module[0] + '.esf.' + url_module[1]               
+                    #print("省份：", province,"城市：", city)
+                    #print("城市链接：", city_url)
+                    #print("新房：", newshouse_url)
+                    #print("二手房", esf_url)                
+                yield scrapy.Request(url=newshouse_url, callback=self.parse_newhouse,
+                                     meta={"info":(province, city)})                                     
+                yield scrapy.Request(url=esf_url, callback=self.parse_esf,
+                                     meta={"info":(province, city)}) 
+
+    def parse_newhouse(self, response):
+        province, city = response.meta.get('info')
+        lis = response.xpath('//div[@id="newhouse_loupai_list"]/ul/li[not(@style)]') 
+        for li in lis:
+            name = li.xpath('.//div[@class="nlcd_name"]/a/text()').get().strip()
+            price = "".join(li.xpath('.//div[@class="nhouse_price"]//text()').getall())
+            price = re.sub(r"\s|广告", "",price)
+            house_type_list = li.xpath('.//div[contains(@class, "house_type")]//text()').getall()
+            house_type_list = re.sub(r"\s", "", "".join(house_type_list)).split('－')
+            rooms = house_type_list[0]
+            area = house_type_list[-1]
+            address = li.xpath('.//div[@class="address"]/a/@title').get()
+            district_text = "".join(li.xpath('.//div[@class="address"]/a//text()').getall()) 
+            district = re.search(r".*\[(.+)\].*", district_text).group(1)
+            sale = response.xpath(".//div[@class='fangyuan pr']/span/text()").get() 
+            origin_url = response.xpath(".//div[@class='nlcd_name']/a/@href").get()
+            origin_url = response.urljoin(origin_url)
+            item = NewHouseItem(province = province, city=city, name=name,
+                                price=price, rooms=rooms, area=area, 
+                                address=address, district=district,
+                                sale=sale, origin_url=origin_url)
+            #print(name, price, rooms, area, address, district, sale, origin_url)
+            yield item
+            
+        next_url = response.xpath("//div[@class='page']//a[@class='next']/@href").get
+        if next_url:
+            yield scrapy.Request(url=response.urljoin(next_url), callback=self.parse_newhouse,
+                                 meta={"info":(province,city)})
+            
+    def parse_esf(self, response):
+        province, city = response.meta.get('info')
+        dls = response.xpath("//div[contains(@class, 'shop_list')]/dl[@dataflag!='bgcomare']")
+        for dl in dls:
+            item = ESFItem(province=province, city=city)
+            item['name'] = dl.xpath(".//p[@class='add_shop']/a/@title").get()  
+            infos = dl.xpath(".//p[@class='tel_shop']//text()").getall()
+            infos = list(map(lambda x:re.sub(r'\s|\|', '', x), infos))
+            info = [x for x in infos if x != '']
+            for info in infos:
+                if "厅" in info:
+                    item['rooms'] = info
+                elif "㎡" in info:
+                    item['area'] = info
+                elif '层' in info:
+                    item['floor'] = info
+                elif "向" in info:
+                    item['toward'] = info
+                elif "年" in info:
+                    item['year'] = info
+                else:
+                    item['username'] = info
+            
+            #item['rooms'] = info[0]
+            #item['area'] = info[1]
+            #item['floor'] = info[2]
+            #item['toward'] = info[3]
+            #item['year'] = info[4]
+            #item['username'] = info[5]            
+            item['address'] = dl.xpath(".//p[@class='add_shop']/span/text()").get() 
+            item['price'] = "".join(dl.xpath(".//dd[@class='price_right']/span[1]//text()").getall())
+            item['unit'] = dl.xpath(".//dd[@class='price_right']/span[2]//text()").get()
+            origin_url = dl.xpath(".//dd/h4[@class='clearfix']/a/@href").get() 
+            origin_url = response.urljoin(origin_url)
+            item['origin_url']= origin_url
+            yield item
+            
+        next_page_url = None   
+        next_urls = response.xpath("//div[@id='list_D10_15']/p/a")
+        for next_url in next_urls:    
+            if next_url.xpath('./text()').get() == "下一页":
+                next_page_url = next_url.xpath("./@href").get()
+                break
+        if next_page_url:
+            yield scrapy.Request(url=response.urljoin(next_page_url), callback=self.parse_esf,
+                                 meta={"info":(province,city)})
